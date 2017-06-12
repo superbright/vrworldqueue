225105444bc557c831339446bbd82768cf1c0598
paul <paul.christophe6@gmail.com>
2017-06-12 12:50:23 -0400

    get users

--- src/client/js/components/AdminList.js
+++ src/client/js/components/AdminList.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
 
 class AdminList extends Component {
+  componentWillMount() {
+    return fetch('/api/users', {
+      method: 'get',
+    }).then((res) => {
+      console.log(res);
+      return res.json()
+    }).then((res) => {
+      console.log(res);
+    }).catch((err) => {
+      console.log('error', err);
+    });
+  }
+
   render() {
     const { match: { params: { id } } } = this.props;
 
