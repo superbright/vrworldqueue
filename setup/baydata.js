ef6fab15ba763f353a92b978f2cc3e0cd1b00de2
igal nassima <inassima@gmail.com>
2017-06-12 12:56:46 -0400

    bay data setup files and config json

---
+++ setup/baydata.js
@@ -0,0 +1,17 @@
+const bays = [
+  {
+    bayid: 0,
+    name: '',
+    game: '',
+    ipAddr: '192.168.1.29',
+  },
+  {
+    bayid: 1,
+    name: '',
+    game: '',
+    ipAddr: '192.168.1.30',
+  },
+];
+module.exports = {
+  bays,
+};
