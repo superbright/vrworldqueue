4dd8aed7dd70a8715ce8a7bbd92568d06d091d84
paul <paul.christophe6@gmail.com>
2017-06-13 19:14:16 -0400

    connect

--- src/client/js/components/AdminUser.js
+++ src/client/js/components/AdminUser.js
@@ -28,7 +28,7 @@ class AdminList extends Component {
         user,
         socket: io('http://localhost:3000', {query: `clientType=admin&clientId=${adminid}` }),
       });
-
+      this.connectSocket();
     }).catch((err) => {
       console.log('error', err);
     });
