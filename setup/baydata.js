bb4304469befb9f51194b054a2bcd929bc111309
Laura Mun <laurasumun@gmail.com>
2017-06-13 12:25:45 -0400

    populated with bay data

--- setup/baydata.js
+++ setup/baydata.js
@@ -1,15 +1,248 @@
 const bays = [
   {
     bayid: 0,
-    name: '',
-    game: '',
-    ipAddr: '192.168.1.29',
+    name: 'A-1',
+    game: 'Play Things',
   },
   {
     bayid: 1,
-    name: '',
+    name: 'A-2',
+    game: 'Fruit Ninja',
+  },
+  {
+    bayid: 2,
+    name: 'A-3',
+    game: 'Tilt Brush',
+  },
+  {
+    bayid: 3,
+    name: 'A-4A',
+    game: 'Icaros',
+  },
+  {
+    bayid: 4,
+    name: 'A-4B',
+    game: 'Icaros',
+  },
+  {
+    bayid: 5,
+    name: 'A-5',
+    game: 'Superhot',
+  },
+  {
+    bayid: 6,
+    name: 'A-6A',
+    game: 'Project Cars',
+  },
+  {
+    bayid: 7,
+    name: 'A-6B',
+    game: 'Project Cars',
+  },
+  {
+    bayid: 8,
+    name: 'A-6C',
+    game: 'Project Cars',
+  },
+  {
+    bayid: 9,
+    name: 'A-6D',
+    game: 'Project Cars',
+  },
+  {
+    bayid: 10,
+    name: 'A-7',
+    game: 'VR Skeet Shooter',
+  },
+  {
+    bayid: 11,
+    name: 'A-8',
+    game: 'Lucky\'s Tale',
+  },
+  {
+    bayid: 12,
+    name: 'A-9',
+    game: 'I Expect You to Die',
+  },
+  {
+    bayid: 13,
+    name: 'B-1',
+    game: 'Brookhaven Experiment',
+  },
+  {
+    bayid: 14,
+    name: 'B-2A',
+    game: 'Raw Data',
+  },
+  {
+    bayid: 15,
+    name: '2-B2',
+    game: 'Raw Data',
+  },
+  {
+    bayid: 16,
+    name: '2-C',
+    game: 'Thrill of the Fight',
+  },
+  {
+    bayid: 17,
+    name: '2-D',
+    game: 'Serious Sam',
+  },
+  {
+    bayid: 18,
+    name: '2-E',
+    game: 'The Climb',
+  },
+  {
+    bayid: 19,
+    name: '2-F',
+    game: 'Rick & Morty',
+  },
+  {
+    bayid: 20,
+    name: '2-G',
+    game: 'Superhot',
+  },
+  {
+    bayid: 21,
+    name: '2-H',
+    game: 'Elven Assassin',
+  },
+  {
+    bayid: 22,
+    name: '2-J',
+    game: 'Space Pirate Trainer',
+  },
+  {
+    bayid: 23,
+    name: '2-K',
+    game: 'Job Simulator',
+  },
+  {
+    bayid: 24,
+    name: '2-L',
+    game: 'Dear Angelica',
+  },
+  {
+    bayid: 25,
+    name: '2-M',
+    game: 'Night Cafe',
+  },
+  {
+    bayid: 26,
+    name: '2-N',
+    game: 'Henry',
+  },
+  {
+    bayid: 27,
+    name: '2-P',
+    game: 'Tilt Brush',
+  },
+  {
+    bayid: 28,
+    name: '2-Q',
+    game: 'Thumper',
+  },
+  {
+    bayid: 29,
+    name: 'M-D1',
+    game: 'Tilt Brush',
+  },
+  {
+    bayid: 30,
+    name: 'M-D2',
+    game: 'Skeet Shooter',
+  },
+  {
+    bayid: 31,
+    name: 'M-A1',
     game: '',
-    ipAddr: '192.168.1.30',
+  },
+  {
+    bayid: 32,
+    name: 'M-A2',
+    game: '',
+  },
+  {
+    bayid: 33,
+    name: 'M-A3',
+    game: '',
+  },
+  {
+    bayid: 34,
+    name: 'M-A4',
+    game: '',
+  },
+  {
+    bayid: 35,
+    name: 'M-A5',
+    game: '',
+  },
+  {
+    bayid: 36,
+    name: 'M-A6',
+    game: '',
+  },
+  {
+    bayid: 37,
+    name: 'M-B1',
+    game: '',
+  },
+  {
+    bayid: 38,
+    name: 'M-B2',
+    game: '',
+  },
+  {
+    bayid: 39,
+    name: 'M-B3',
+    game: '',
+  },
+  {
+    bayid: 40,
+    name: 'M-B4',
+    game: '',
+  },
+  {
+    bayid: 41,
+    name: 'M-B5',
+    game: '',
+  },
+  {
+    bayid: 42,
+    name: 'M-B6',
+    game: '',
+  },
+  {
+    bayid: 43,
+    name: 'M-C1',
+    game: '',
+  },
+  {
+    bayid: 44,
+    name: 'M-C2',
+    game: '',
+  },
+  {
+    bayid: 45,
+    name: 'M-C3',
+    game: '',
+  },
+  {
+    bayid: 46,
+    name: 'M-C4',
+    game: '',
+  },
+  {
+    bayid: 47,
+    name: 'M-C5',
+    game: '',
+  },
+  {
+    bayid: 48,
+    name: 'M-C6',
+    game: '',
   },
 ];
 module.exports = {
