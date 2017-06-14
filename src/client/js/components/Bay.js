ffd6f76de3202e5cc16b910dc297e73f20ef15ac
paul <paul.christophe6@gmail.com>
2017-06-14 18:50:26 -0400

    removing queue from bay

--- src/client/js/components/Bay.js
+++ src/client/js/components/Bay.js
@@ -33,23 +33,6 @@ class Bay extends Component {
               <header className="flex space-between align-center">
                 <h5>{bay.name}</h5>
               </header>
-              {
-                bay.queue.length === 0
-                ? (
-                  <div className="simple-container user-search"><h3>{'There\'s no one in line, register now!'}</h3></div>
-                )
-                : (
-                  <ul>
-                    {
-                      bay.queue.map(player => (
-                        <li className="user-list-item flex space-between align-center">
-                          <div>{player.screenname}</div>
-                        </li>
-                      ))
-                    }
-                  </ul>
-                )
-              }
             </div>
           )
         }
