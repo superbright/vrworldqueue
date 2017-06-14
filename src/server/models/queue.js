e7363e59abedcc5c15dcea1b331139ea1a439ebf
Nate Turley <Nate Turley>
2017-06-14 16:08:29 -0400

    actually add queue model

---
+++ src/server/models/queue.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+var schema = new mongoose.Schema({
+    users: [{
+        user: {
+            type: mongoose.Schema.Types.ObjectId
+            , ref: 'User'
+        }
+        , timeAdded: {
+            type: Date
+            , default: Date.now
+        }
+        , _id: false
+    }]
+});
+var Queue = mongoose.model('Queue', schema);
+module.exports = {
+    Queue: Queue
+}
