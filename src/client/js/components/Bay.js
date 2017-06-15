389f2c177f49eb827cc2e95e0b86a7c3fbe21b85
paul <paul.christophe6@gmail.com>
2017-06-15 10:22:48 -0400

    clean up react

--- src/client/js/components/Bay.js
+++ src/client/js/components/Bay.js
@@ -1,88 +1,85 @@
-import React, {
-    Component
-}
-from 'react';
+import React, { Component } from 'react';
 import io from 'socket.io-client';
+
 class Bay extends Component {
-    constructor() {
-        super();
-        this.state = {
-            bay: null
-            , socket: null
-            , queue: []
-        };
-    }
-    componentWillMount() {
-        const {
-            match: {
-                params: {
-                    bayid
-                }
-            }
-        } = this.props;
-        return fetch(`/api/bays/${bayid}`, {
-            method: 'get'
-        , }).then(res => res.json()).then((bay) => {
-            this.setState({
-                bay, socket: io('http://localhost:3000', {
-                    query: `clientType=queue&clientId=${bayid}`
-                })
-            });
-            this.connectSocket();
-            return fetch(`/api/bays/${bayid}/queue`, {
-                method: 'get'
-            }).then(res => res.json()).then((queue) => {
-                console.log(queue)
-                this.setState({
-                    queue
-                })
-            }).catch((err) => {
-                console.log('error', err);
-            })
-        }).catch((err) => {
-            console.log('error', err);
+  constructor() {
+    super();
+    this.state = {
+      bay: null,
+      socket: null,
+      queue: [],
+    };
+  }
+  componentWillMount() {
+    const { match: { params: { bayid } } } = this.props;
+    return fetch(`/api/bays/${bayid}`, {
+      method: 'get',
+    }).then(res => res.json()).then((bay) => {
+      this.setState({
+        bay,
+        socket: io('http://localhost:3000', {
+          query: `clientType=queue&clientId=${bayid}`,
+        }),
+      });
+
+      this.connectSocket();
+
+      return fetch(`/api/bays/${bayid}/queue`, {
+        method: 'get',
+      }).then(res => res.json()).then((queue) => {
+        this.setState({
+          queue,
         });
+      }).catch((err) => {
+        console.log('error', err);
+      });
+    }).catch((err) => {
+      console.log('error', err);
+    });
+  }
+  connectSocket() {
+    const { socket } = this.state;
+    if (socket) {
+      socket.on('queue', (res) => {
+        this.setState({ queue: res });
+      });
     }
-    connectSocket() {
-        const {
-            socket
-        } = this.state;
-        if (socket) {
-            console.log('--', socket);
-            socket.on('queue', (res) => {
-                console.log('Queue Message', res);
-                this.setState({
-                    queue: res
-                });
-            });
+  }
+  render() {
+    const { bay, queue } = this.state;
+    const { match: { params: { bayid } } } = this.props;
+
+    return (
+      <div key={bayid}>
+        {
+          bay && (
+            <div>
+              <header className="flex space-between align-center">
+                <h5>{bay.name}</h5>
+              </header>
+              {
+                queue.length === 0
+                ? (
+                  <div className="simple-container user-search">
+                    <h3>{'There\'s no one in line, register now!'}</h3>
+                  </div>
+                )
+                : (
+                  <ul> {
+                    queue.map(player => (
+                      <li className="user-list-item flex space-between align-center">
+                        <div>{player.user.screenname}</div>
+                      </li>
+                    ))}
+                  </ul>
+                )
+              }
+            </div>
+          )
         }
-    }
-    render() {
-            const {
-                bay, queue
-            } = this.state;
-            const {
-                match: {
-                    params: {
-                        bayid
-                    }
-                }
-            } = this.props;
-            return ( < div key = {
-                        bayid
-                    } > {
-                        bay && ( < div > < header className = "flex space-between align-center" > < h5 > {
-                                bay.name
-                            } < /h5> < /header > {
-                                queue.length === 0 ? ( < div className = "simple-container user-search" > < h3 > {
-                                    'There\'s no one in line, register now!'
-                                } < /h3></div > ) : ( < ul > {
-                                        queue.map(player => ( < li className = "user-list-item flex space-between align-center" > < div > {
-                                            player.user.screenname
-                                        } < /div> < /li > ))
-                                    } < /ul>)
-                                } < /div>)
-                            } < /div>);
-                        }
-                    }
-                    export default Bay;
+      </div>
+    );
+  }
+}
+
+export default Bay;
