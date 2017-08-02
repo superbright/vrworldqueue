4a6f8c673aecc362f2c0790b9a063bee4c9bb714
baruchlane <baruchlane@users.noreply.github.com>
2017-08-02 10:48:10 -0400

    Revert "Adding Other Gender and Optional Flag"

--- dist/server/models/user.js
+++ dist/server/models/user.js
@@ -20,7 +20,7 @@ var schema = new mongoose.Schema({
     },
     gender: {
         type: String,
-        emum: ['male', 'female', 'other']
+        emum: ['male', 'female']
     },
     dob: {
         type: Date
